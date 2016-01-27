b653d7d94f5a3f88ae1ddcfa01a91e6d5f1abfee
mytreo <mytreogm@gmail.com>
2016-01-28 01:43:25 +0200

    add test response helper

--- src/main/java/ua/mytreo/java/soltest/servlets/ChangeBookServlet.java
+++ src/main/java/ua/mytreo/java/soltest/servlets/ChangeBookServlet.java
@@ -1,9 +1,15 @@
 package ua.mytreo.java.soltest.servlets;
 
+import org.xml.sax.SAXException;
+import ua.mytreo.java.soltest.entity.Catalog;
+import ua.mytreo.java.soltest.parser.Parser;
+import ua.mytreo.java.soltest.parser.impl.ParserJaxbImpl;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.bind.JAXBException;
 import java.io.BufferedReader;
 import java.io.IOException;
 
@@ -11,7 +17,7 @@ import java.io.IOException;
  * Сервлет,отвечающий за работу со списком книг
  *
  * @author mytreo   27.01.2016.
- * @version 1.1
+ * @version 1.2
  */
 public class ChangeBookServlet extends HttpServlet {
     public void doPost(HttpServletRequest request,
@@ -35,12 +41,28 @@ public class ChangeBookServlet extends HttpServlet {
         }
         value = workBuffer.toString();
 
-        if (!value.equals(""))
-            response.getWriter().println(value);
-
+        response.getWriter().println(responseHelper(value));
 
         response.setContentType("text/xml;charset=utf-8");
         response.setStatus(HttpServletResponse.SC_OK);
 
+    }
+
+    private String responseHelper(String reqXml){
+        if (reqXml.equals("")) return "all catalog";
+        Parser parser;
+        Catalog catalog;
+        try {
+             parser= new ParserJaxbImpl();
+        } catch (SAXException e) {
+            return "parser error";
+        }
+        try {
+            catalog=(Catalog)parser.unMarshall(reqXml,Class.forName("ua.mytreo.java.soltest.entity.Catalog"));
+        } catch (JAXBException |ClassNotFoundException e) {
+            e.printStackTrace();
+            catalog=new Catalog();
+        }
+        return catalog.toString();
     }
 }
